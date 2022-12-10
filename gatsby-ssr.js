19a06d9b246b60a144c1e54e688283cc026c885b
supermarine1377 <supermarine@outlook.jp>
2022-12-10 12:55:19 +0900

    fix gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,12 @@
+const React = require("react")
+
 exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: "ja" })
   setHeadComponents([
     <script 
       async 
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8654869064923371"
-      crossOrigin="anonymous"></script>
+      crossOrigin="anonymous">
+    </script>
   ])
 }
