19f8edb4589ca12d6bd5c33f346f78e15934adfd
supermarine1377 <supermarine@outlook.jp>
2022-12-10 12:50:27 +0900

    add google adcense to gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,9 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: "ja" })
+  setHeadComponents([
+    <script 
+      async 
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8654869064923371"
+      crossOrigin="anonymous"></script>
+  ])
 }
