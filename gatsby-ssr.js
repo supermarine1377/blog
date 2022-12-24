0cc30b5afa1ffc01bd213db18db71450fd003062
supermarine1377 <supermarine@outlook.jp>
2022-12-24 17:18:20 +0900

    add ads

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,10 +3,22 @@ const React = require("react")
 exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: "ja" })
   setHeadComponents([
-    <script 
-      async 
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8654869064923371"
-      crossOrigin="anonymous">
-    </script>
+    <>
+      <script 
+        async 
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8654869064923371"
+        crossOrigin="anonymous">
+      </script>
+      <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-8654869064923371"
+        data-ad-slot="1856430621"
+        data-ad-format="auto"
+        data-full-width-responsive="true">
+        </ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+    </>
   ])
 }
