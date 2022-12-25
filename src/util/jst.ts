ae6d36039f112a37dee363813a25347c100f445f
supermarine1377 <53213350+supermarine1377@users.noreply.github.com>
2022-12-25 19:38:56 +0900

    fix (#35)

--- src/util/jst.ts
+++ src/util/jst.ts
@@ -2,8 +2,8 @@ import dayjs from 'dayjs'
 
 const toFormattedJst = (utc: string) => {
   const date = new Date(utc)
-  const jst = dayjs(date).add(9, 'h')
-  return dayjs(jst).format("YYYY年MM月DD日")
+  // jst
+  return dayjs(date).format("YYYY年MM月DD日")
 }
 
 export default toFormattedJst
