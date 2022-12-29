40d514d4b4d026376b602753926409d5e4de6e10
supermarine1377 <supermarine@outlook.jp>
2022-12-30 00:19:46 +0900

    add privacy-policy page

--- src/pages/privacy_policy.jsx
+++ src/pages/privacy_policy.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { graphql} from 'gatsby'
+import Layout from "../components/layout"
+import Main from "../components/main"
+import * as style from "./privacy_policy.module.css"
 
-const PrivacyPolicy = () => {
-
+const PrivacyPolicy = ({ data }) => {
+  const html = data.allContentfulPrivacyPolicy.edges[0].node.description.childMarkdownRemark.html
+  return (
+    <Layout>
+      <Main>
+        <section className={style.section} dangerouslySetInnerHTML={{__html: html}} />
+      </Main>
+    </Layout>
+  )
 }
 
 export default PrivacyPolicy
+
+export const aboutmeQuery = graphql`
+  query{
+    allContentfulPrivacyPolicy {
+      edges {
+        node {
+          title
+          description {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+      }
+    }
+  }
+`
