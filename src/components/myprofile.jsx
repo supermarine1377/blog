c915fee305a2d763af7c83de92cbe7c48ed92aad
supermarine1377 <supermarine@outlook.jp>
2023-01-01 01:17:26 +0900

    refactoring

--- src/components/myprofile.jsx
+++ src/components/myprofile.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 import { GatsbyImage } from 'gatsby-plugin-image'
 import * as styles from './myprofile.module.css'
-import { StaticImage } from "gatsby-plugin-image"
+import MyTwitter from "./mytwitter"
+import NinkiBlogRanking from "./ninki_blog_ranking"
+import NihonBlogMura from "./nhon_blog_mura"
 
 const MyProfile = ({ title, topImage, description }) => {
   return (
@@ -21,29 +23,9 @@ const MyProfile = ({ title, topImage, description }) => {
           {description}
         </p>
         <div className={styles.links}>
-          <a href="https://investment.blogmura.com/americatoushi/ranking/in?p_cid=11158132" target="_blank" rel="noopener noreferrer" aria-label="にほんブログ村へ">
-            <StaticImage 
-              src="../images/banner.jpg"
-              width={88}
-              height={31} 
-              alt={"にほんブログ村のロゴ"}
-            />
-          </a>
-          <a href="https://blog.with2.net/link/?id=2096540&tag=56" target="_brank" rel="noopener noreferrer" aria-label="「#投資」人気ブログランキング">
-            <StaticImage
-              src="../images/logo_ninki.png"
-              width={149}
-              height={30}
-              alt={"にほんブログ村のロゴ"}
-            />
-          </a>
-          <a href="https://twitter.com/us_investing137" target="_blank" rel="noopener noreferrer" aria-label="公式Twitterアカウントへ">
-            <StaticImage
-              src="../images/twitter.png"
-              height={26}
-              alt={"Twitterのロゴ"}
-            />
-          </a>
+          <NihonBlogMura />
+          <NinkiBlogRanking />
+          <MyTwitter />
         </div>
       </div>
     </header>
