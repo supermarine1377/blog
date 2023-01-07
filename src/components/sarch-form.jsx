b4f8408be2b295101046bebed15bae10e82cefbb
supermarine1377 <53213350+supermarine1377@users.noreply.github.com>
2023-01-07 18:37:14 +0900

    fix: strategy of Gatsby Script API (#52)

--- src/components/sarch-form.jsx
+++ src/components/sarch-form.jsx
@@ -4,7 +4,7 @@ import { Script } from "gatsby";
 const SearchForm = () => {
   return (
     <>
-      <Script async src="https://cse.google.com/cse.js?cx=339ed3c3548d74069" />
+      <Script strategy="idle" src="https://cse.google.com/cse.js?cx=339ed3c3548d74069" />
       <div className="gcse-search" />
     </>
   )
