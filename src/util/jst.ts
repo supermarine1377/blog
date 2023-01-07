ec2cabf7d9c95025e695c16e9c6f95514acfc020
supermarine1377 <supermarine@outlook.jp>
2023-01-07 14:21:27 +0900

    fix: emove new Date()

--- src/util/jst.ts
+++ src/util/jst.ts
@@ -1,9 +1,8 @@
 import dayjs from 'dayjs'
 
 const toFormattedJst = (utc: string) => {
-  const date = new Date(utc)
   // jst
-  return dayjs(date).format("YYYY年MM月DD日")
+  return dayjs(utc).format("YYYY年MM月DD日")
 }
 
 export default toFormattedJst
