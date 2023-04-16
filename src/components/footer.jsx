4f71d20a9afbc4183b81e1dade48036b583e7593
supermarine1377 <53213350+supermarine1377@users.noreply.github.com>
2023-04-16 14:06:52 +0900

    footer追加 (#64)

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as styles from "./footer.module.css"
+import useSiteMetadata from "../hooks/use-site-metadata";
+
+const Footer = () =>  {
+  const { contactPageUrl } = useSiteMetadata()
+  return (
+    <footer className={styles.footer}>
+      © 2022 いがらし <a target="blank" rel="noopener noreferrer" href={contactPageUrl} className={styles.link}>お問い合わせ</a>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navigation from "./navigation";
 import './variables.css'
 import './global.css'
+import Footer from "./footer";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navigation />
       {children}
+      <Footer />
     </>
   )
 }
