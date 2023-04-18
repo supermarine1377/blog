c1f854215127f7e7e9b2439a0a1fb5137cc698b9
supermarine1377 <supermarine@outlook.jp>
2023-04-18 18:50:35 +0900

    adsスクリプト変更

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,20 +4,23 @@ exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: "ja" });
   setHeadComponents([
     <>
-      <script
-        async
+      <script 
+        async 
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8654869064923371"
-        crossOrigin="anonymous"
-      ></script>
-      <ins
+        crossorigin="anonymous">
+      </script>
+      <ins 
         class="adsbygoogle"
         style={{ display: "block" }}
         data-ad-client="ca-pub-8654869064923371"
-        data-ad-slot="1856430621"
+        data-ad-slot="1563441994"
         data-ad-format="auto"
         data-full-width-responsive="true"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      >
+      </ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
     </>,
   ]);
 };
