a8f9db8b637c12ef95ae7ce5f86d178212a5d124
supermarine1377 <53213350+supermarine1377@users.noreply.github.com>
2023-04-18 18:59:27 +0900

    add ads (#34)
    * add ads
    
    * fix
    
    * adsスクリプト変更

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,26 @@
-const React = require("react")
+const React = require("react");
 
 exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
-  setHtmlAttributes({ lang: "ja" })
-  // setHeadComponents([
-  //   <script 
-  //     async 
-  //     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8654869064923371"
-  //     crossOrigin="anonymous">
-  //   </script>
-  // ])
-}
+  setHtmlAttributes({ lang: "ja" });
+  setHeadComponents([
+    <>
+      <script 
+        async 
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8654869064923371"
+        crossorigin="anonymous">
+      </script>
+      <ins 
+        class="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-8654869064923371"
+        data-ad-slot="1563441994"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      >
+      </ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+    </>,
+  ]);
+};
