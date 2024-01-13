2276555a72940851bf0fe9dfb0c28b48bae204b5
supermarine1377 <supermarine@outlook.jp>
2024-01-14 00:12:29 +0900

    記事内広告削除

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -71,17 +71,6 @@ export const Head = (ctx) => {
           twitterAccount: twitterAccount
         }}
       />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8654869064923371"
-        crossorigin="anonymous"></script>
-      <ins class="adsbygoogle"
-        style={{display: "block", textAlign: "center"}}
-        data-ad-layout="in-article"
-        data-ad-format="fluid"
-        data-ad-client="ca-pub-8654869064923371"
-        data-ad-slot="5540978973"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({ });
-      </script>
     </>
   )
 }
