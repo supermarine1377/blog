993c4417f72f3e4c0ea62c66097082a68329137e
supermarine1377 <supermarine@outlook.jp>
2024-01-13 20:26:13 +0900

    fix

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -74,7 +74,7 @@ export const Head = (ctx) => {
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8654869064923371"
         crossorigin="anonymous"></script>
       <ins class="adsbygoogle"
-        style="display:block; text-align:center;"
+        style={{display: "block", textAlign: "center"}}
         data-ad-layout="in-article"
         data-ad-format="fluid"
         data-ad-client="ca-pub-8654869064923371"
